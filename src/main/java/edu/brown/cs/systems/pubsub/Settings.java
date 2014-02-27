cc6bf935f02b407aeb11ec614d9c17063a747846
Jonathan Mace <jonathan.c.mace@gmail.com>
2014-02-26 20:12:48 -0800

    Expanded the configuration to make it easier to set up for multiple machines

--- src/main/java/edu/brown/cs/systems/pubsub/Settings.java
+++ src/main/java/edu/brown/cs/systems/pubsub/Settings.java
@@ -1,28 +1,131 @@
 package edu.brown.cs.systems.pubsub;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.List;
+
 import akka.actor.ActorSystem;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 public class Settings {
-  
-  private static final int[] SEED_PORTS = new int[] { 2551, 2552, 2553, 2554, 2555, 2556, 2557, 2558, 2559 };
+    
+    private static final String PUBSUB_HOSTS_KEY = "pubsub.hosts";
+    private static final String PUBSUB_PORTS_KEY = "pubsub.ports";
 
-  public static ActorSystem getActorSystem() {
-    for (int port : SEED_PORTS) {
-      try {
-        Config config = ConfigFactory.parseString("akka.remote.netty.tcp.port="+port).withFallback(ConfigFactory.load());
-        ActorSystem system = ActorSystem.create("PubSub", config);
-        System.out.println("Created PubSub ActorSystem on port " + port);
-        return system;
-      } catch (Exception e) {
-        System.out.println("Unable to create PubSub actor system on port " + port);
-      }
+    /**
+     * Gets the PubSub actor system.
+     * seed hosts are configured with the pubsub.hosts string list
+     * seed ports are configured with the pubsub.ports int list
+     * If the current machine is not in the config, then the
+     * local inet address is used.
+     * @return
+     */
+    public static ActorSystem getActorSystem() {
+        // Expand all of the pubsub seed nodes
+        Config loadedConfig = Settings.withSeedNodes(ConfigFactory.load());
+        printPubSubConfig(loadedConfig);
+        
+        // Get this machine's IP address and a list of seed node ports
+        String ip = Settings.getLocalAddress(loadedConfig);
+        List<Integer> seeds = Settings.getCandidatePorts(loadedConfig);
+        
+        // Try to start on one of the seed ports
+        for (Integer port : seeds) {
+            try {
+                return createActorSystem(ip, port, loadedConfig);
+            } catch (Exception e) {
+                System.out.println("Unable to create PubSub actor system at " + ip + ":" + port);
+            }
+        }
+        
+        // Start on a random port
+        return createActorSystem(ip, 0, loadedConfig);
     }
-    ActorSystem system = ActorSystem.create("PubSub", ConfigFactory.load());
-    System.out.println("Created PubSub ActorSystem on random port");
-    return system;
-  }
-  
+    
+    public static ActorSystem createActorSystem(String ip, Integer port, Config conf) {
+        Config config = Settings.atLocalAddress(ip, port, conf);
+        ActorSystem system = ActorSystem.create("PubSub", config);
+        System.out.println("Created PubSub actor system at " + ip + ":" + port);
+        return system;
+    }
+    
+    public static Config atLocalAddress(String ip, int port, Config conf) {
+        String nettyconf = String.format("akka.remote.netty.tcp { hostname = \"%s\", port = %d }", ip, port);
+        return ConfigFactory.parseString(nettyconf).withFallback(conf);
+    }
+    
+    public static Config withSeedNodes(Config conf) {
+        StringBuilder builder = new StringBuilder();
+        List<String> hosts = conf.getStringList("pubsub.hosts");
+        List<Integer> ports = conf.getIntList("pubsub.ports");
+        builder.append("akka.cluster.seed-nodes=[");
+        boolean first = true;
+        for (String host : hosts) {
+            for (Integer port : ports) {
+                if (!first)
+                    builder.append(",");
+                first = false;
+                builder.append("\"akka.tcp://PubSub@");
+                builder.append(host);
+                builder.append(":");
+                builder.append(port);
+                builder.append("\"");
+            }
+        }
+        builder.append("]");
+        return ConfigFactory.parseString(builder.toString()).withFallback(conf);
+    }
+    
+    public static String getLocalAddress(Config conf) {
+        for (String candidate : conf.getStringList(PUBSUB_HOSTS_KEY)) {
+            if (isAddressOrMachineLocal(candidate))
+                return candidate;
+        }
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return  "127.0.0.1";
+        }
+    }
+    
+    public static List<Integer> getCandidatePorts(Config conf) {
+        List<Integer> ports = conf.getIntList(PUBSUB_PORTS_KEY);
+        ports.add(0);
+        return ports;
+    }
+
+    public static boolean isAddressOrMachineLocal(String addr) {
+        try {
+            return isIPAddressLocal(InetAddress.getByName(addr));
+        } catch (UnknownHostException e) {
+            System.out.println("UnknownHostException: " + addr);
+            return false;
+        }
+    }
+
+    public static boolean isIPAddressLocal(InetAddress addr) {
+        if (addr.isAnyLocalAddress() || addr.isLoopbackAddress())
+            return true;
+
+        try {
+            return NetworkInterface.getByInetAddress(addr) != null;
+        } catch (SocketException e) {
+            return false;
+        }
+    }
+    
+    public static void printPubSubConfig(Config conf) {
+        String prt = "PubSub seed hosts: ";
+        for (String host : conf.getStringList(PUBSUB_HOSTS_KEY))
+            prt += host + " ";
+        prt += "\nPubSub seed ports: ";
+        for (Integer port : conf.getIntList(PUBSUB_PORTS_KEY))
+            prt += port + " ";
+        System.out.println(prt);
+    }
+
 }

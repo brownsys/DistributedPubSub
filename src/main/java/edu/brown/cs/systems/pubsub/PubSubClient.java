8d831845f15544af675ee421026dc72b36f37c35
Jonathan Mace <jonathan.c.mace@gmail.com>
2014-02-28 14:30:40 -0800

    Print stack trace on exception in callback handler

--- src/main/java/edu/brown/cs/systems/pubsub/PubSubClient.java
+++ src/main/java/edu/brown/cs/systems/pubsub/PubSubClient.java
@@ -96,6 +96,7 @@ public class PubSubClient {
                                 c.OnMessage(m);
                             } catch (Exception e) {
                                 System.out.println("Exception calling callback for message: " + m);
+                                e.printStackTrace();
                             }
                         }
                     }

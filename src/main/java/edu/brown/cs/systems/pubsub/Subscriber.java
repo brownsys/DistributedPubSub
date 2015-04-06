9c62075ccfa001baf6c4c67b9682145e765fb7e4
Jonathan Mace <jonathan.c.mace@gmail.com>
2015-04-06 12:27:53 -0400

    Mark subscriber as deprecated

--- src/main/java/edu/brown/cs/systems/pubsub/Subscriber.java
+++ src/main/java/edu/brown/cs/systems/pubsub/Subscriber.java
@@ -5,9 +5,11 @@ import com.google.protobuf.Message;
 /**
  * Legacy subscriber class. Still here because it defines the callback class
  */
+@Deprecated
 public interface Subscriber {
 
   /** Legacy callback class. Extends edu.brown.cs.systems.pubsub.Callback */
+  @Deprecated
   public abstract class Callback<T extends Message> extends
       edu.brown.cs.systems.pubsub.client.impl.Callback<T> {
   }
--- src/test/java/edu/brown/cs/systems/pubsub/TestPubSub.java
+++ src/test/java/edu/brown/cs/systems/pubsub/TestPubSub.java
@@ -11,36 +11,35 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 import edu.brown.cs.systems.pubsub.PubSubProtos.StringMessage;
-import edu.brown.cs.systems.pubsub.Subscriber.Callback;
 import edu.brown.cs.systems.pubsub.client.PubSubClient;
 import edu.brown.cs.systems.pubsub.client.PubSubClientFactory;
+import edu.brown.cs.systems.pubsub.client.impl.Callback;
 import edu.brown.cs.systems.pubsub.server.PubSubServer;
 import edu.brown.cs.systems.pubsub.server.PubSubServerFactory;
 
 public class TestPubSub {
 
-  
-  
-  private static final class TestPubSubCallback extends Callback<PubSubProtos.StringMessage> {
-    
+  private static final class TestPubSubCallback extends
+      Callback<PubSubProtos.StringMessage> {
+
     private static final int wait_time = 1000;
-    
+
     public volatile String msg = null;
     private Semaphore semaphore = new Semaphore(0);
-    
+
     public void awaitMessage(String msg) throws InterruptedException {
       assertTrue(semaphore.tryAcquire(wait_time, TimeUnit.MILLISECONDS));
       assertTrue(msg.equals(this.msg));
       reset();
     }
-    
+
     public void awaitNoMessage(String msg) throws InterruptedException {
       assertFalse(semaphore.tryAcquire(wait_time, TimeUnit.MILLISECONDS));
       assertFalse(msg.equals(this.msg));
       assertNull(this.msg);
-      reset();      
+      reset();
     }
-    
+
     public void reset() {
       semaphore.drainPermits();
       msg = null;
@@ -51,51 +50,58 @@ public class TestPubSub {
       msg = message.getMessage();
       semaphore.release();
     }
-    
+
   }
-  
+
   @Test
   public void testBPubSub() throws InterruptedException, IOException {
     String topic = "test";
-    
+
     PubSubServer server = PubSubServerFactory.create();
     PubSubClient client = PubSubClientFactory.create();
-    
+
     // Subscribe to test topic
     TestPubSubCallback cb = new TestPubSubCallback();
     client.subscribe(topic, cb);
     Thread.sleep(1000);
-    
+
     // Publish a message
-    System.out.println("Publisher publishing " + "hello" + " on topic " + topic);
-    client.publish(topic, PubSubProtos.StringMessage.newBuilder().setMessage("hello").build());
+    System.out
+        .println("Publisher publishing " + "hello" + " on topic " + topic);
+    client.publish(topic,
+        PubSubProtos.StringMessage.newBuilder().setMessage("hello").build());
     cb.awaitMessage("hello");
-    
+
     // Publish a message
-    System.out.println("Publisher publishing " + "hello" + " on topic " + "badtest");
-    client.publish("badtest", PubSubProtos.StringMessage.newBuilder().setMessage("hello").build());
+    System.out.println("Publisher publishing " + "hello" + " on topic "
+        + "badtest");
+    client.publish("badtest", PubSubProtos.StringMessage.newBuilder()
+        .setMessage("hello").build());
     cb.awaitNoMessage("hello");
-    
+
     // Publish a message
-    System.out.println("Publisher publishing " + "hello2" + " on topic " + topic);
-    client.publish(topic, PubSubProtos.StringMessage.newBuilder().setMessage("hello2").build());
+    System.out.println("Publisher publishing " + "hello2" + " on topic "
+        + topic);
+    client.publish(topic,
+        PubSubProtos.StringMessage.newBuilder().setMessage("hello2").build());
     cb.awaitMessage("hello2");
-    
+
     // Unsubscribe
     client.unsubscribe(topic, cb);
     Thread.sleep(1000);
-    
+
     // Publish a message
-    System.out.println("Publisher publishing " + "hello2" + " on topic " + topic);
-    client.publish(topic, PubSubProtos.StringMessage.newBuilder().setMessage("hello2").build());
+    System.out.println("Publisher publishing " + "hello2" + " on topic "
+        + topic);
+    client.publish(topic,
+        PubSubProtos.StringMessage.newBuilder().setMessage("hello2").build());
     cb.awaitNoMessage("hello2");
-    
+
     // Close the server and client
     server.shutdown();
     client.shutdown();
     server.awaitTermination();
     client.awaitTermination();
   }
-
 
 }
